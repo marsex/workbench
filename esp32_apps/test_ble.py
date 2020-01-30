8e7d71e6992c028b1f28920fa19aa979350197ae
marsex <marsex@protonmail.com>
2020-01-30 12:37:21 -0300

    Update

--- esp32_apps/test_ble.py
+++ esp32_apps/test_ble.py
@@ -7,7 +7,21 @@ ble.active(True)
 # Event Handling
 def ble_irq(event, data):
     # print received data
-    print(ble.gatts_read(rx))
+    try:
+        conn_handle, addr_type, addr = data
+    except:
+        conn_handle, addr_type = data
+
+    print('\nevent:',event)
+        
+    if event == 1:
+        print('USER:',addr,'connected')
+    elif event == 2:
+        print('USER:',addr,'disconnected')
+        start_ble()
+    elif event == 4:
+        print('RX:',ble.gatts_read(rx))
+        ble.gatts_write(tx,'test')
         
 ble.irq(ble_irq)
 
@@ -19,9 +33,14 @@ UART_SERVICE = (UART_UUID, (UART_TX, UART_RX,),)
 SERVICES = (UART_SERVICE,)
 ((tx, rx,), ) = ble.gatts_register_services(SERVICES)
 
-# Advertiser
+def stop_ble():
+    print('stop ble')
+    ble.gap_advertise(0)
+
+def start_ble():
+    print('start ble')
+    ble.gap_advertise(100, bytearray('\x02\x01\x02') + adv_encode_name('ESP32'))
+
 def adv_encode_name(name):
     name = bytes(name, 'ascii')
     return bytearray((len(name) + 1, 0x09)) + name
-
-ble.gap_advertise(100, bytearray('\x02\x01\x02') + adv_encode_name('ESP32'))
